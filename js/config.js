d15b95690a78898a9c10eb4f84aa9749a8a7e411
JJ110112 <106655662+JJ110112@users.noreply.github.com>
2025-05-17 19:49:20 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// 系統設定
+const CONFIG = {
+  // 請替換為您的 Google Apps Script Web App URL
+  API_URL: 'https://script.google.com/macros/s/AKfycbzBydYi14NWQSPfcb7_yYIKCO1C4CgC5waUdBVSU9lD7pDQU2vF5Dgg03j4XPoHxEEW/exec',
+  
+  // 區域設定
+  AREAS: ['南中正', '景美', '木柵'],
+  
+  // 系統名稱
+  SYSTEM_NAME: '罷免志工班表系統',
+  
+  // 版本
+  VERSION: '1.0.0'
+};
+
+// 匯出設定
+window.CONFIG = CONFIG;
